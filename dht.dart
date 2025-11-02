c535e4ccaff38fa1764a5ae96f36b448d31ca4b0
Huypro2 <nitnamehappy@gmail.com>
2025-11-02 10:58:02 +0700

    Add files via upload

---
+++ dht.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+import 'package:json_annotation/json_annotation.dart';
+part 'dht.g.dart';
+
+@JsonSerializable()
+class Dht {
+  dynamic tempC;
+  dynamic humi;
+  dynamic count;
+  dynamic date;
+
+  ///ngày thang
+  Dht(this.tempC, this.humi, this.count);
+
+  ///this.date);
+  factory Dht.fromJson(Map<String, dynamic> json) => _$DhtFromJson(json);
+  Map<String, dynamic> toJson() => _$DhtToJson(this);
+}
---
+++ dht.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dht.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Dht _$DhtFromJson(Map<String, dynamic> json) =>
+    Dht(json['tempC'], json['humi'], json['count'])..date = json['date'];
+
+Map<String, dynamic> _$DhtToJson(Dht instance) => <String, dynamic>{
+  'tempC': instance.tempC,
+  'humi': instance.humi,
+  'count': instance.count,
+  'date': instance.date,
+};
